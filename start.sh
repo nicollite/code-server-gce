49a65049bb18ded5a1b4a9669a239acf4272ab08
root <root@dev.c.nicolas-dev-2.internal>
2020-09-11 20:12:20 +0000

    Change start.sh script

--- start.sh
+++ start.sh
@@ -14,7 +14,7 @@ function get-pid(){
 }
 
 # If a code server pid exists kill it
-codeServerPid=$(get-port-pid $CODE_SERVER_PORT)
+codeServerPid=$(get-port-pid 8000)
 if [ -n "$codeServerPid" ]; then
   printf "killing old code server pid: $codeServerPid \n\n"
   sudo kill $codeServerPid
@@ -23,17 +23,22 @@ else
 fi
 
 # Start a new code server
-printf "starting new code server on port $CODE_SERVER_PORT!\n\n"
-sudo -E nohup code-server --config ~/code-server/config.yaml > "code-server.log" 2>&1 &
+printf "starting new code server on port 8000!\n\n"
+sudo -E nohup code-server --config ./config.yaml > "code-server.log" 2>&1 &
 
 # Print the used configurations
-printf "code-server config path ~/code-server/config.yaml\n$(cat ~/code-server/config.yaml) \n\n"
+codeServerConfigPath=$(ls "`pwd`/config.yaml")
+printf "code-server config path `ls $codeServerConfigPath`\n$(cat $codeServerConfigPath) \n\n"
 
 # Sleep for 2 seconds and get the new 
 
-pid for the code server
+# pid for the code server
 sleep 2s
-newCodeServerPid=$(get-port-pid $CODE_SERVER_PORT)
-msg="code server running on port: $newCodeServerPid"
-echo $msg >> code-server.log
-echo $msg
+newCodeServerPid=$(get-port-pid 8000)
+codeServerPortMsg="code server running on port: $newCodeServerPid"
+echo $codeServerPortMsg >> code-server.log
+echo $codeServerPortMsg
+
+# Get the VM external ip address
+vmIp=$(curl -H "Metadata-Flavor: Google" http://metadata.google.internal/computeMetadata/v1/instance/network-interfaces/0/access-configs/0/external-ip)
+echo "You can access the vscode terminal in http://$vmIp"
